49c6c73c23fba6344b30d4ee5c806df78c8db65d
Thomas P George <tomgeorge1307@gmail.com>
2024-10-01 03:30:39 +0530

    second commit

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,4 +1,4 @@
 export const useGetUserInfo = () =>{
-    const {userID, name, profilePhoto, isAuth } = JSON.parse(localStorage.getItem("auth"))
+    const {userID, name, profilePhoto, isAuth } = JSON.parse(localStorage.getItem("auth")) || {}
     return {userID, name, profilePhoto, isAuth};
 }
